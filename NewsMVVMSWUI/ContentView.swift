46e2fa3c43d7d55d262740d9bdfde6430e7463cb
Jose Bustos <35240264+korbek99@users.noreply.github.com>
2026-02-01 19:36:04 -0300

    Initial Commit

---
+++ NewsMVVMSWUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NewsMVVMSWUI
+//
+//  Created by Jose Preatorian on 01-02-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NewsMVVMSWUI/NewsMVVMSWUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsMVVMSWUIApp.swift
+//  NewsMVVMSWUI
+//
+//  Created by Jose Preatorian on 01-02-26.
+//
+
+import SwiftUI
+
+@main
+struct NewsMVVMSWUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
